5562e316c24e818c21cd6e0e457dfe8454db84bf
Guillaume Lamourec <parkerpool@live.fr>
2025-04-03 00:39:09 +0200

    added a button for erasing input content

--- src/components/SearchAccountForm/SearchAccountForm.jsx
+++ src/components/SearchAccountForm/SearchAccountForm.jsx
@@ -1,25 +1,62 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./SearchAccountForm.module.css";
 import useResizeObserver from "../../hooks/useResizeObserver";
 
 export default function SearchAccountForm({ handleSubmit }) {
+  const [inputEmpty, setInputEmpty] = useState(true);
+  const inputRef = useRef(null);
   const buttonRef = useRef(null);
   const buttonWidth = useResizeObserver(buttonRef, "width");
+
+  let timeoutId = null;
+  const resetInputWithStyle = () => {
+    const inputValue = inputRef.current.value;
+    if (inputValue) {
+      inputRef.current.value = inputValue.slice(0, inputValue.length - 1);
+      timeoutId = setTimeout(() => {
+        resetInputWithStyle();
+      }, 30);
+    }
+    setInputEmpty(true);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId !== null) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [timeoutId]);
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type="text"
         name="search"
+        ref={inputRef}
         aria-label="searchbar"
         placeholder="DJKFifou"
+        onChange={(e) => setInputEmpty(!e.target.value)}
       />
+      {!inputEmpty && (
+        <button
+          type="button"
+          aria-label="Erase"
+          title="Erase"
+          className={styles.eraseBtn}
+          onClick={resetInputWithStyle}
+        >
+          ×
+        </button>
+      )}
       <button
-        aria-label="search"
+        type="submit"
+        aria-label="Search"
         ref={buttonRef}
+        className={styles.searchBtn}
         onMouseUp={(e) => e.target.blur()}
       >
-        {buttonWidth > 40 ? "Rechercher" : ""}
+        {buttonWidth > 40 ? "Search" : ""}
         <div
           className={styles.searchIcon}
           onMouseUp={(e) => e.target.parentElement.blur()}
